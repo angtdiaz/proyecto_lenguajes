50bc3a678c1a33ed218579ab85010ed6858ba7e3
angtdiaz <nicoltuarezd@gmail.com>
2021-11-14 14:47:17 -0500

    Angie Tuarez

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -1,18 +1,20 @@
 import ply.lex as lex
-## moises coronel =>
-tokens = ['VARIABLE','VARIABLE_GLOBAL','VARIABLE_INSTANCIA','VARIABLE_CLASE','CONSTANTE','COMENTARIO','CADENA','ENTERO','FLOTANTE']
+# moises coronel / modificación angie tuarez
+tokens = ['VARIABLE', 'VARIABLE_GLOBAL', 'VARIABLE_INSTANCIA', 'VARIABLE_CLASE',
+          'CONSTANT', "FLOATINGPOINT", "INTEGER", "BOOLEAN", "LETTER", "STRING", "PLUS", "MINUS",
+          "TIMES", "DIVIDE", "AND", "OR", "NOT", "EQUALS", "LESS", "GREATER", "LPAREN", "RPAREN",
+          "COMMA", "SEMICOLON", "APOST", "QUOTE", "LBRACE", "RBRACE", "LBRACKET", "RBRACKET", "COMMENT", "POUND", "ISEQUAL"]
 
 
-## Palabras reservadas (moises coronel)
+# Palabras reservadas (moises coronel)
 reserved = {
 
     'elsif': 'ELSIF',
+    'else': 'ELSE',
+    'if': 'IF',
     'def': 'DEF',
     'class': 'CLASS',
-    'and': 'AND',
-    'or': 'OR',
     'do': 'DO',
-    'else': 'ELSE',
     'while': 'WHILE',
     'unless': 'UNLESS',
     'end': 'END',
@@ -21,14 +23,10 @@ reserved = {
     'case': 'CASE',
     'in': 'IN',
     'raise': 'RAISE',
-    'false': 'FALSE',
     'for': 'FOR',
-    'if': 'IF',
     'nil': 'NIL',
     'redo': 'REDO',
-    'true': 'TRUE',
     'return': 'RETURN',
-    'not': 'NOT',
     'self': 'SELF',
     'retry': 'RETRY'
 }
@@ -39,27 +37,70 @@ tokens = tokens + list(reserved.values())
 # TOKENS (Moises Coronel)
 
 #t_VARIABLE = r'[a-z][A-Za-z0-9_]*'
-t_VARIABLE_GLOBAL= r'\$[a-zA-Z_][A-Za-z0-9_]*'
-t_VARIABLE_INSTANCIA=r'\@[a-zA-Z_][A-Za-z0-9_]*'
-t_VARIABLE_CLASE=r'\@\@[a-zA-Z_][A-Za-z0-9_]*'
-t_CONSTANTE= r'[A-Z][a-z]*'
-t_CADENA = r'(\"|\')[a-zA-z0-9\s]*(\"|\')'
-t_COMENTARIO = r'(\#.*|\"\"\".*\"\"\")'
-##------------------------
-## moises coronel
-def t_VARIABLE(t):
-    r'[a-zA-Z_][A-Za-z0-9_]*'
-    t.type = reserved.get(t.value,'VARIABLE')
+t_VARIABLE_GLOBAL = r'\$[a-zA-Z_][A-Za-z0-9_]*'
+t_VARIABLE_INSTANCIA = r'\@[a-zA-Z_][A-Za-z0-9_]*'
+t_VARIABLE_CLASE = r'\@\@[a-zA-Z_][A-Za-z0-9_]*'
+t_CONSTANT = r'[A-Z][a-z]*'
+# t_STRING = r'(\"|\')[a-zA-z0-9\s]*(\"|\')'
+t_COMMENT = r'(\#.*|\"\"\".*\"\"\")'
+# ------------------------
+
+# angie tuarez
+t_PLUS = r"\+"
+t_MINUS = r"\-"
+t_TIMES = r"\*"
+t_DIVIDE = r"\/"
+t_AND = r"\&\&"
+t_OR = r"\|\|"
+t_NOT = r"\!"
+t_ISEQUAL = r"\=\="
+t_LESS = r"\<"
+t_GREATER = r"\>"
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_COMMA = r"\,"
+t_SEMICOLON = r"\;"
+t_APOST = r"\'"
+t_QUOTE = r"\""
+t_LBRACE = r"\{"
+t_RBRACE = r"\}"
+t_LBRACKET = r"\["
+t_RBRACKET = r"\]"
+t_POUND = r"\#"
+t_EQUALS = r"\="
+
+
+def t_BOOLEAN(t):
+    r"true|false"
     return t
 
- ## moises coronel
-def t_FLOTANTE(t):
+
+def t_STRING(t):
+    r"\".*\""
+    return t
+
+
+def t_LETTER(t):
+    r"\'.\'"
+    t.value = t.value.replace("", "")
+    return t
+
+# moises coronel
+
+
+def t_VARIABLE(t):
+    r'[a-zA-Z_][A-Za-z0-9_]*'
+    t.type = reserved.get(t.value, 'VARIABLE')
+    return t
+
+
+def t_FLOATINGPOINT(t):
     r'-?\d+\.\d+'
     t.value = float(t.value)
     return t
 
- ## moises coronel
-def t_ENTERO(t):
+
+def t_INTEGER(t):
     r'-?[0-9]+'
     t.value = int(t.value)
     return t
@@ -69,12 +110,12 @@ def t_ENTERO(t):
 t_ignore = " \t"
 
 
-## moises coronel
+# moises coronel
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
 
- ## moises coronel
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
